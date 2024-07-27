5ca853bbfac408668516077da0f1eb3c89809494
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-26 21:01:35 -0300

    adicionando as configs para o webcomponent

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  bootstrapApplication,
+  createApplication,
+} from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { createCustomElement } from '@angular/elements';
+import { CatDetailsModalComponent } from './app/components/cat-details-modal/cat-details-modal.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+(async () => {
+  const app = await createApplication({
+    providers: [importProvidersFrom(MatDialogModule, BrowserAnimationsModule)],
+  });
+
+  const catDetailsModalElement = createCustomElement(CatDetailsModalComponent, {
+    injector: app.injector,
+  });
+  customElements.define('cat-details-modal', catDetailsModalElement);
+
+  bootstrapApplication(AppComponent, appConfig).catch((err) =>
+    console.error(err)
+  );
+})();
