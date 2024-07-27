83273d3121687eddce952c2b797fc2149262ad7f
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-26 21:01:59 -0300

    ajustando imports

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../header/header.component';
+import { HeaderComponent } from '../header/header.component';
 import { GalleryComponent } from '../gallery/gallery.component';
 
 @Component({
