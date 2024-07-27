e7c57eee1c80796f038bc9b85884635e6c52c272
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-27 18:20:56 -0300

    removendo codigo nao utilizado

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,7 @@ import {
 } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { createCustomElement } from '@angular/elements';
-import { CatDetailsModalComponent } from './app/components/cat-details-modal/cat-details-modal.component';
 
-(async () => {
-  const app = await createApplication({
-    providers: [importProvidersFrom(MatDialogModule, BrowserAnimationsModule)],
-  });
-
-  const catDetailsModalElement = createCustomElement(CatDetailsModalComponent, {
-    injector: app.injector,
-  });
-  customElements.define('cat-details-modal', catDetailsModalElement);
-
-  bootstrapApplication(AppComponent, appConfig).catch((err) =>
-    console.error(err)
-  );
-})();
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
