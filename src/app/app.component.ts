6ac677b5704e82a9aa8fabfd161ce26bdc9bc41d
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-27 19:46:37 -0300

    criando do webcomponent de formulário

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Injector } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { createCustomElement } from '@angular/elements';
+import { FormService } from './components/form/form.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  providers: [FormService],
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'adote-um-gato';
+
+  constructor(injector: Injector) {
+    const FormElement = createCustomElement(FormComponent, {
+      injector,
+    });
+    customElements.define('form-element', FormElement);
+  }
 }
---
+++ src/app/components/form/form.component.ts
@@ -0,0 +1,21 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent {
+  @Output() formSubmitted = new EventEmitter<void>();
+  @Input() title!: string;
+
+  onSubmit(form: NgForm) {
+    console.log('onSubmit');
+    if (form.valid) {
+      this.formSubmitted.emit();
+    }
+  }
+}
