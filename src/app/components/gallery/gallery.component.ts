a9399a6b1bb917d4a06f3ec45c3ed530ca62fff8
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-28 20:43:57 -0300

    adicionando log na requisicao

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -4,6 +4,7 @@ import { CatsData } from '../../interfaces/cat.interface';
 import { CatApiService } from '../../services/cat-api.service';
 import { Router } from '@angular/router';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { NGXLogger } from 'ngx-logger';
 
 @Component({
   selector: 'app-gallery',
@@ -14,10 +15,14 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 })
 export class GalleryComponent implements OnInit {
   catsData: CatsData[] = [];
-  error: string | null = null;
+  error = false;
   isLoading = true;
 
-  constructor(private catApiService: CatApiService, private router: Router) {}
+  constructor(
+    private catApiService: CatApiService,
+    private router: Router,
+    private logger: NGXLogger
+  ) {}
 
   ngOnInit(): void {
     this.loadCatImages();
@@ -31,8 +36,8 @@ export class GalleryComponent implements OnInit {
       },
       error: (err) => {
         this.isLoading = false;
-        this.error = 'Failed to load cat images';
-        console.error(err);
+        this.error = true;
+        this.logger.error('Falha na requisição', err);
       },
     });
   }
