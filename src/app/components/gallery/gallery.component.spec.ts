ff2711ce104bb3e6f8b7fbacc29c7712e493186a
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-29 14:55:35 -0300

    atualizando testes

--- src/app/components/gallery/gallery.component.spec.ts
+++ src/app/components/gallery/gallery.component.spec.ts
@@ -9,6 +9,7 @@ import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { GalleryComponent } from './gallery.component';
 import { By } from '@angular/platform-browser';
 import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { CatsData } from '../../interfaces/cat.interface';
 
 describe('GalleryComponent', () => {
   let component: GalleryComponent;
@@ -74,11 +75,17 @@ describe('GalleryComponent', () => {
     ).nativeElement;
     expect(gridWrapper).toBeTruthy();
 
-    const imageElements = gridWrapper.getElementsByTagName('img');
+    const imageElements = gridWrapper.getElementsByClassName('cat-image');
     expect(imageElements.length).toBe(3);
-    expect(imageElements[0].src).toContain('https://example.com/cat1.jpg');
-    expect(imageElements[1].src).toContain('https://example.com/cat2.jpg');
-    expect(imageElements[2].src).toContain('https://example.com/cat3.jpg');
+    expect((imageElements[0] as HTMLImageElement).src).toContain(
+      'https://example.com/cat1.jpg'
+    );
+    expect((imageElements[1] as HTMLImageElement).src).toContain(
+      'https://example.com/cat2.jpg'
+    );
+    expect((imageElements[2] as HTMLImageElement).src).toContain(
+      'https://example.com/cat3.jpg'
+    );
   }));
 
   describe('When the component is loading', () => {
@@ -94,8 +101,16 @@ describe('GalleryComponent', () => {
     });
   });
   describe('When the component is not loading', () => {
-    it('the spinner should be displayed', () => {
+    it('the spinner should not be displayed', () => {
       component.isLoading = false;
+      component.catsData = [
+        {
+          url: 'test-url',
+          breeds: [{ name: 'Test Breed' }],
+          details: 'Test Details',
+        },
+      ] as CatsData[];
+
       fixture.detectChanges();
 
       const galleryWrapper: HTMLElement = fixture.debugElement.query(
