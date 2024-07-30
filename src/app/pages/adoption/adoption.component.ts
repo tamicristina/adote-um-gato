cb5d9f2537c7f249055f6723f5aea8bcada752da
Tamires <79336444+tamicristina@users.noreply.github.com>
2024-07-30 14:07:45 -0300

    adicionando componentes de formulários

--- src/app/pages/adoption/adoption.component.ts
+++ src/app/pages/adoption/adoption.component.ts
@@ -2,11 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgIf } from '@angular/common';
 import { FormComponent } from '../../webcomponents/form/form.component';
+import { CustomInputComponent } from '../../webcomponents/form/components/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-adoption',
   standalone: true,
-  imports: [NgIf, FormComponent],
+  imports: [NgIf, FormComponent, CustomInputComponent],
   templateUrl: './adoption.component.html',
   styleUrl: './adoption.component.scss',
 })
--- src/app/webcomponents/form/form.component.ts
+++ src/app/webcomponents/form/form.component.ts
@@ -3,29 +3,53 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnInit,
   Output,
-  ViewChild,
+  ContentChildren,
+  QueryList,
 } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { CustomInputComponent } from './components/custom-input/custom-input.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [FormsModule, NgIf],
+  imports: [FormsModule, NgIf, CustomInputComponent, ReactiveFormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss',
 })
-export class FormComponent {
+export class FormComponent implements OnInit {
+  form: FormGroup = new FormGroup({
+    label: new FormControl(''),
+  });
+
+  constructor(private fb: FormBuilder) {}
   @Output() formSubmitted = new EventEmitter<void>();
+
   @Input() title!: string;
   @Input() buttonLabel!: string;
   @Input() catName!: string;
 
-  @ViewChild('f') form!: NgForm;
+  @ContentChildren(CustomInputComponent)
+  customInputs!: QueryList<CustomInputComponent>;
 
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      this.formSubmitted.emit();
-    }
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      pet: ['', Validators.required],
+      mensagem: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitted.emit();
   }
 }
